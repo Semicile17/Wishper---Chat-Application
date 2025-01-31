e9bb1a33f2c7bac88a1de2612124cda72d46332f
Semicile17 <Rohitsinghmahant707@gmail.com>
2025-01-31 21:04:25 +0530

    fixing errors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://wishper-chat-application-jw4s.vercel.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://wishper-chat-application-jw4s.vercel.app/api" : "https://wishper-chat-application-jw4s.vercel.app/api",
   withCredentials: true,
 });
