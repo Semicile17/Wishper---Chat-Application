addb3c036c329f4f3257a8dba2954ddb926f207c
Semicile17 <Rohitsinghmahant707@gmail.com>
2025-01-31 13:57:20 +0530

    added vercel.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../dist", "index.html"));
   });
 
 
