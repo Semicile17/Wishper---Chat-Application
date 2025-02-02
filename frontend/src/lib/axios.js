462d261878bf4069ffda352875d32f051a44c3a2
Semicile17 <Rohitsinghmahant707@gmail.com>
2025-02-02 11:08:10 +0530

    code optimised for render deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://wishper-chat-application-jw4s.vercel.app/api" : "https://wishper-chat-application-jw4s.vercel.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
